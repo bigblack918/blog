ac7836073e16d9c0e5a2e2adc74be8b3f55c5bb0
i_leotsai1 <leotsai1@fareastone.com.tw>
2018-06-11 15:03:19 +0800

    Init blog

---
+++ content/index_test.md
@@ -0,0 +1,8 @@
+Title: test
+Date: 2018-06-11 13:55
+Category: main
+tags: welcome, python
+
+## Pelican is great
+
+Wrdditting our blog like writting our code is awesome
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Leo'
+SITENAME = "Leo's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = 'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('GitHup', 'https://github.com/bigblack918/'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = 'FLEX'
